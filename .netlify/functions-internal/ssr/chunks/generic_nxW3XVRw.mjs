bd8fff96352f242bc181b1aa643f5f1d6961e3e0
Jailerwebwork <jailerwebwork@gmail.com>
2024-01-03 20:50:13 -0500

    layout2

--- .netlify/functions-internal/ssr/chunks/generic_nxW3XVRw.mjs
+++
@@ -1,6 +0,0 @@
-export { renderers } from '../renderers.mjs';
-export { onRequest } from '../_empty-middleware.mjs';
-
-const page = () => import('./pages/generic_-kcbOvRg.mjs').then(n => n.g);
-
-export { page };
